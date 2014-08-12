1794c38a5e44a0ca2b10f0ed503df862156d6c65
JohnMBarry <john.m.barry@hotmail.com>
2014-08-12 13:21:05 +1000

    [Cellarise] Deploy release 0.1.1

--- Test/resources/simple.js
+++
@@ -1,4 +0,0 @@
-//jshint ignore:start
-var author = "John Barry";
-var name = "gulp-dust-compile-render";
-var description = "A gulp task to compile and render dust templates based on a provided context object.";
